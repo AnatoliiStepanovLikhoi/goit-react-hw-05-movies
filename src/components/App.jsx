2efc1f1f6d49250a53a50258b829f65fc5985f16
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-01-10 22:01:09 +0200

    added header and movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Header } from './Header';
+import Movies from 'pages/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}></Route>
+        <Route path="movies" element={<Movies />}></Route>
+      </Routes>
+    </>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,12 @@
+export default function Movies() {
+  return (
+    <>
+      <form>
+        <label>
+          <input type="text" name="findMovie" />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </>
+  );
+}
