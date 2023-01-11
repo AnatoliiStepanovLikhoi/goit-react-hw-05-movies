5768e722f8e369ba0884dfe00431f71a19eb210a
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-01-11 12:01:14 +0200

    added layout, header, movies, home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { Header } from './Header';
+// import { Header } from './Layout/Header/Header';
 import Movies from 'pages/Movies';
+import Home from 'pages/Home';
+
+import { Layout } from './Layout/Layout';
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Header />}></Route>
-        <Route path="movies" element={<Movies />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="movies" element={<Movies />}></Route>
+          {/* <Route path="movies/:movieId" element={<MovieDetail />}>
+                  <Route path="cast" element={<Cast />}></Route>
+                  <Route path="reviews" element={<Reviews />}></Route>
+          </Route> */}
+        </Route>
       </Routes>
     </>
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
   );
 };
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+
 export default function Movies() {
+  const [inputRequest, setInputRequest] = useState('');
+
+  const onInputSubmit = event => {
+    event.preventDefault();
+
+    console.log(inputRequest);
+  };
+
+  const onInputUpdate = ({ target }) => {
+    setInputRequest(target.value);
+
+    // console.log(target.value);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={onInputSubmit}>
         <label>
-          <input type="text" name="findMovie" />
+          <input type="text" name="findMovie" onChange={onInputUpdate} />
         </label>
         <button type="submit">Search</button>
       </form>
